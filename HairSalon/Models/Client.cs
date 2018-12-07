01529901ecaa9cd5e7366b70ce256ffa249b5226
James Cho <hwisoocho@gmail.com>
2018-12-07 10:42:07 -0800

    add client logic

---
+++ HairSalon/Models/Client.cs
@@ -0,0 +1,212 @@
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+
+namespace HairSalon.Models
+{
+  public class Client
+  {
+    private string _name;
+    private int _phone;
+    private int _id;
+    private int _stylistId;
+
+    public Client (string name, int phone, int id = 0, int stylistId)
+    {
+      _name = name;
+      _phone = phone;
+      _id = id;
+      _stylistId = stylistId;
+    }
+
+    public string GetName()
+    {
+      return _name;
+    }
+
+
+    public int GetId()
+    {
+      return _id;
+    }
+
+    public int GetStylistId()
+    {
+      return _stylistId;
+    }
+
+    public static List<Client> GetAll()
+    {
+      List<Client> allClients = new List<Client> {};
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM clients;";
+      var rdr = cmd.ExecuteReader() as MySqlDataReader;
+      while(rdr.Read())
+      {
+        string clientName = rdr.GetString(0);
+        int clientPhone = rdr.GetInt32(1);
+        int clientId = rdr.GetInt32(2);
+        int stylistId = rdr.GetInt32(3);
+        Client newClient = new Client(clientName, clientPhone, clientId, stylistId);
+        allClients.Add(newClient);
+      }
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+      return allClients;
+    }
+
+    public static List<Client> GetAllStylistClients(int id)
+    {
+      List<Client> stylistClients = new List<Client> {};
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM clients WHERE stylist_id = '" + id + "';";
+      var rdr = cmd.ExecuteReader() as MySqlDataReader;
+      while(rdr.Read())
+      {
+         string clientName = rdr.GetString(0);
+        int clientPhone = rdr.GetInt32(1);
+        int clientId = rdr.GetInt32(2);
+        int stylistId = rdr.GetInt32(3);
+        Client newClient = new Client(clientName, clientPhone, clientId, stylistId);
+        stylistClients.Add(newClient);
+      }
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+      return stylistClients;
+    }
+
+    public static void ClearAll()
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"DELETE FROM clients;";
+      cmd.ExecuteNonQuery();
+      conn.Close();
+      if (conn != null)
+      {
+       conn.Dispose();
+      }
+    }
+
+    public static void DeleteClient(int id)
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"DELETE FROM clients WHERE id = (@thisId);";
+      
+      MySqlParameter thisId = new MySqlParameter();
+      thisId.ParameterName = "@thisId";
+      thisId.Value = id;
+      cmd.Parameters.Add(thisId);
+      cmd.ExecuteNonQuery();
+      conn.Close();
+      if (conn != null)
+      {
+       conn.Dispose();
+      }
+    }
+
+    public static Item Find(int id)
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM clients WHERE id = (@searchId);";
+      MySqlParameter searchId = new MySqlParameter();
+      searchId.ParameterName = "@searchId";
+      searchId.Value = id;
+      cmd.Parameters.Add(searchId);
+      var rdr = cmd.ExecuteReader() as MySqlDataReader;
+      int itemId = 0;
+      string itemName = "";
+      int itemCategoryId = 0;
+      while(rdr.Read())
+      {
+        itemId = rdr.GetInt32(0);
+        itemName = rdr.GetString(1);
+        itemCategoryId = rdr.GetInt32(2);
+      }
+      Item newItem = new Item(itemName, itemCategoryId, itemId);
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+      return newItem;
+    }
+
+    public override bool Equals(System.Object otherItem)
+    {
+      if (!(otherItem is Item))
+      {
+        return false;
+      }
+      else
+      {
+         Item newItem = (Item) otherItem;
+         bool idEquality = this.GetId() == newItem.GetId();
+         bool descriptionEquality = this.GetDescription() == newItem.GetDescription();
+         bool categoryEquality = this.GetCategoryId() == newItem.GetCategoryId();
+         return (idEquality && descriptionEquality && categoryEquality);
+       }
+    }
+
+    public void Save()
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"INSERT INTO items (description, category_id) VALUES (@description, @category_id);";
+      MySqlParameter description = new MySqlParameter();
+      description.ParameterName = "@description";
+      description.Value = this._description;
+      cmd.Parameters.Add(description);
+      MySqlParameter categoryId = new MySqlParameter();
+      categoryId.ParameterName = "@category_id";
+      categoryId.Value = this._categoryId;
+      cmd.Parameters.Add(categoryId);
+      cmd.ExecuteNonQuery();
+      _id = (int) cmd.LastInsertedId;
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+    }
+
+    public void Edit(string newDescription)
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"UPDATE items SET description = @newDescription WHERE id = @searchId;";
+      MySqlParameter searchId = new MySqlParameter();
+      searchId.ParameterName = "@searchId";
+      searchId.Value = _id;
+      cmd.Parameters.Add(searchId);
+      MySqlParameter description = new MySqlParameter();
+      description.ParameterName = "@newDescription";
+      description.Value = newDescription;
+      cmd.Parameters.Add(description);
+      cmd.ExecuteNonQuery();
+      _description = newDescription; // <--- This line is new!
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+    }
+
+  }
+}
